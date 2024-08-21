9961d60c458763f468198c48ffe7c9960573edf3
subashini08 <49692871+subashini08@users.noreply.github.com>
2024-08-21 01:50:06 -0500

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  description = "Deployment environment (e.g., test, prod)"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
